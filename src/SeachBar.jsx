6163c76b85af65ba085ec1206742629b68a03d10
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-03-25 18:03:32 +0100

    Restyle the search icon

--- src/SeachBar.jsx
+++ src/SeachBar.jsx
@@ -18,7 +18,18 @@ export default function SearchBar() {
     <Form onSubmit={handleSubmit} className="bg-dark">
       <InputGroup className="mb-3">
         <InputGroup.Text style={{ backgroundColor: "transparent" }}>
-          <Image src="/icon/search.svg" style={{ width: "20px" }} />
+          <svg
+            className="search-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g data-name="Layer 2">
+              <g data-name="search">
+                <rect width="24" height="24" opacity="0" />
+                <path d="M20.71 19.29l-3.4-3.39A7.92 7.92 0 0 0 19 11a8 8 0 1 0-8 8 7.92 7.92 0 0 0 4.9-1.69l3.39 3.4a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 11a6 6 0 1 1 6 6 6 6 0 0 1-6-6z" />
+              </g>
+            </g>
+          </svg>
         </InputGroup.Text>
         <Form.Control
           className="bg-dark text-white"
